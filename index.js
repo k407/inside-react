90153a86acf06dce7eb51c797502e42e698c1695
k407 <k40788@gmail.com>
2022-04-23 02:20:40 +0500

    Replace Storage with Redux

--- index.js
+++ index.js
@@ -134,43 +134,7 @@ function loadingReducer(state = loadingInitialState) {
   return state;
 }
 
-function combineReducer(reducers) {
-  return (state = {}, action) => {
-    const result = {};
-    Object.entries(reducers).forEach(([key, reducer]) => {
-      result[key] = reducer(state[key], action);
-    });
-    return result;
-  };
-}
-
-class Store {
-  constructor(reducer, initialState) {
-    this.reducer = reducer;
-    this.state = reducer(initialState, { type: null });
-    this.listeners = [];
-  }
-
-  getState() {
-    return this.state;
-  }
-
-  subscribe(listener) {
-    this.listeners.push(listener);
-
-    return () => {
-      const index = this.listeners.indexOf(listener);
-      this.listeners.splice(index, 1);
-    };
-  }
-
-  dispatch(action) {
-    this.state = this.reducer(this.state, action);
-    this.listeners.forEach((listener) => listener());
-  }
-}
-
-const store = new Store(combineReducer({
+const store = Redux.createStore(Redux.combineReducers({
   clock: clockReducer,
   products: productsReducer,
   loading: loadingReducer,
